e4587184a049e245077d95670fda01488fae7a76
Anders Jakobsen <andersjkbsn@gmail.com>
2018-03-21 23:58:29 +0100

    Fixed bug in threaded worker

--- travelscanner/crawlers/threaded_worker.py
+++ travelscanner/crawlers/threaded_worker.py
@@ -3,10 +3,10 @@ from concurrent.futures import ThreadPoolExecutor
 
 def crawl_multi_threaded(crawl_function, start_page, max_workers=5):
     ret_travels = set()
-    workers = []
     current_page = start_page
 
     while True:
+        workers = []
         initial_length = len(ret_travels)
 
         with ThreadPoolExecutor(max_workers=max_workers) as executor:
@@ -25,13 +25,14 @@ def crawl_multi_threaded(crawl_function, start_page, max_workers=5):
                 continue
 
             # Check if there were travels from this worker
-            if len(worker_travels) == 0:
+            # Also check if the intersection of existing travels is equal in length with the new travels
+            if len(worker_travels) == 0 or len(ret_travels.intersection(worker_travels)) == len(worker_travels):
                 had_empty_travels = True
             else:
                 ret_travels.update(worker_travels)
 
         # Break the loop if empty travel sets were found
-        if had_empty_travels or initial_length == len(ret_travels):
+        if had_empty_travels:
             break
 
     return ret_travels
