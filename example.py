d596a7261ee0d6a694c7a631c732fab284c0c353
Anders Jakobsen <andersjkbsn@gmail.com>
2018-03-28 15:34:26 +0200

    Set departure date for testing

--- example.py
+++ example.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
     agent = Agent()
     options = agent.get_travel_options()
     options.departure_airports = [Airports.AALBORG, Airports.BILLUND, Airports.COPENHAGEN]
-    options.set_earliest_departure_date('20/06/2018')
+    options.set_earliest_departure_date('13/07/2018')
     options.number_of_guests = 2
     options.max_days_from_departure = 3
 
