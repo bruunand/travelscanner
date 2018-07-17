c08edead3f581ad3f9484e2b0d64a21fc9ea47a9
Anders Jakobsen <andersjkbsn@gmail.com>
2018-07-17 18:38:53 +0200

    Increased crawling distance

--- run_agent.py
+++ run_agent.py
@@ -15,8 +15,7 @@ if __name__ == '__main__':
     agent = Agent()
     options = agent.get_travel_options()
     options.departure_airports = [Airports.AALBORG, Airports.BILLUND, Airports.COPENHAGEN]
-    options.set_earliest_departure_date('27/07/2018')
-    options.set_latest_departure_date('08/08/2018')
+    options.set_latest_departure_date('01/09/2018')
     options.number_of_guests = 2
 
     # Add crawlers
